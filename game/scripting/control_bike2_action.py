ba0f07debdaac7440d888b9647c74cb74a67a08b
Grant Holley <grant.holley98@gmail.com>
2022-03-24 09:37:32 -0600

    bike action classes

--- game/scripting/control_bike2_action.py
+++ game/scripting/control_bike2_action.py
@@ -21,9 +21,7 @@ class ControlActorsAction(Action):
             keyboard_service (KeyboardService): An instance of KeyboardService.
         """
         self._keyboard_service = keyboard_service
-        self._direction_1 = Point(constants.CELL_SIZE, 0)
-        self._direction_2 = Point(constants.CELL_SIZE, 0)
-        self.bikes = []
+        self._direction = Point(constants.CELL_SIZE, 0)
 
     def execute(self, cast, script):
         """Executes the control actors action.
@@ -32,40 +30,6 @@ class ControlActorsAction(Action):
             cast (Cast): The cast of Actors in the game.
             script (Script): The script of Actions in the game.
         """
-        # print('\n\n\n\n', cast, '\n\n\n\n')
-
-        self.bikes.append(cast.get_actors('bike_1'))
-        self.bikes.append(cast.get_actors('bike_2'))
-        for bike in self.bikes:
-            if cast.get_actors('bike_1') == bike:
-                self._bike_1(cast)
-            else:
-                self._bike_2(cast)
-
-    def _bike_1(self, cast):
-        
-        # left
-        if self._keyboard_service.is_key_down('a'):
-            self._direction = Point(-constants.CELL_SIZE, 0)
-        
-        # right
-        if self._keyboard_service.is_key_down('d'):
-            self._direction = Point(constants.CELL_SIZE, 0)
-        
-        # up
-        if self._keyboard_service.is_key_down('w'):
-            self._direction = Point(0, -constants.CELL_SIZE)
-        
-        # down
-        if self._keyboard_service.is_key_down('s'):
-            self._direction = Point(0, constants.CELL_SIZE)
-
-                
-        bike = cast.get_first_actor('bike_1')
-        bike.turn_head(self._direction_1)
-
-    
-    def _bike_2(self, cast):
         # left
         if self._keyboard_service.is_key_down('j'):
             self._direction = Point(-constants.CELL_SIZE, 0)
@@ -83,5 +47,6 @@ class ControlActorsAction(Action):
             self._direction = Point(0, constants.CELL_SIZE)
         
                 
-            bike = cast.get_first_actor('bike_2')
-            bike.turn_head(self._direction_2)
+        bike = cast.get_first_actor('bike_2')
+        bike.turn_head(self._direction)
+
